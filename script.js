f573bbec5426f9a8cfe085b6e70f4cf0f6c15e7f
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-21 09:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function updateTime(){
 	const month = String(now.getMonth()+1).padStart(2,"0");
 	const day = String(now.getDate()).padStart(2,"0");
 	const hour = now.getHours();
-	const formatedHours = hours % 12 || 12;
+	const formattedHours = hours % 12 || 12;
 	const ampm = hours >= 12 ? "PM" : "AM";
 	const minutes = String(now.getMinutes()).padStart(2,"0");
-	const seconds = String(now.getSeconds()).padStart(2'"0");
-		const formattedTime = '${month},${day},${year},${formattedHours}:${minutes}:${seconds}:${ampm}'
+	const seconds = String(now.getSeconds()).padStart(2,"0");
+		const formattedTime = '${month}/${day}/${year},${formattedHours}:${minutes}:${seconds}:${ampm}'
 timerElement.textContent = formattedTime;
 }
 updateTime();
-setIntervals(updateTime,1000);
+setInterval(updateTime, 1000);
