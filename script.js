b37ee8daaab3d60506c48a6311572cd8426187a8
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-21 09:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function updateTime(){
 	const timerElement = document.getElementById("timer");
 	const now = new Date();
 	const year = now.getFullYear();
-	const month = String(now.getMonth()+1).padStart(2,"0");
+	const month = String(now.getMonth() + 1).padStart(2,"0");
 	const day = String(now.getDate()).padStart(2,"0");
-	const hour = now.getHours();
+	const hours = now.getHours();
 	const formattedHours = hours % 12 || 12;
 	const ampm = hours >= 12 ? "PM" : "AM";
 	const minutes = String(now.getMinutes()).padStart(2,"0");
