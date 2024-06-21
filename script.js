7c9e8fb4c673e08d39265d9e71a7e9f83a145d0c
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-21 10:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function updateTime(){
 	const ampm = hours >= 12 ? "PM" : "AM";
 	const minutes = String(now.getMinutes()).padStart(2,"0");
 	const seconds = String(now.getSeconds()).padStart(2,"0");
-		const formattedTime = `${month}/${day}/${year}, ${formattedHours}:${minutes}:${seconds}:${ampm}`
-timerElement.textContent = formattedTime;
+		const formattedTime = `${month}/${day}/${year}, ${formattedHours}:${minutes}:${seconds} ${ampm}`
+  timerElement.textContent = formattedTime;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+	
